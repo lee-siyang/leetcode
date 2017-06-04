ce5a3536abd499149ee154de93fad87ba037a1f0
lee-siyang <lee_siyang@163.com>
2017-06-04 21:30:15 +0800

    solution

---
+++ src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args){
+        int[] arr = new int[2840];
+        Arrays.fill(arr,1);
+
+        int[] a = {1,1,1,0,1,1,1,1,0,0,0,1};
+        int res = findMaxConsecutiveOnes(a);
+        System.out.println(res);
+
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int len = nums.length;
+        int max = 0;
+        int  maxHere= 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[i]==1) {
+                maxHere++;
+                max=Math.max(max, maxHere);
+            } else {
+                maxHere=0;
+            }
+        }
+        return max;
+    }
+}
