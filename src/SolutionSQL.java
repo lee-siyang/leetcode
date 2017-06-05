d181135a52ae17b3d3cf7987f504bfb5c0fa242a
lee-siyang <lee_siyang@163.com>
2017-06-05 23:39:13 +0800

    comment2

---
+++ src/SolutionSQL.java
@@ -0,0 +1,11 @@
+/**
+ * Created by lisiyang on 17/6/5.
+ */
+public class SolutionSQL {
+    //595. Big Countries
+    //SELECT name,population,area FROM World WHERE area > 3000000 OR population > 25000000;
+
+    //182. Duplicate Emails
+    //SELECT Email FROM Person GROUP by Email having count(Email)>1
+
+}
