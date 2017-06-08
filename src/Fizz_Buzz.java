5ef811263630e17b3317e5b9cf4467d695d7164b
lee-siyang <lee_siyang@163.com>
2017-06-08 23:22:54 +0800

    comment2

---
+++ src/Fizz_Buzz.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * Created by lisiyang on 17/6/8.
+ */
+public class Fizz_Buzz {
+    public static void main(String[] args) {
+        int n = 15;
+        List<String> res = fizzBuzz(n);
+        System.out.println(res);
+
+    }
+    public static List<String> fizzBuzz(int n) {
+        List<String> res =new ArrayList<String>(n);
+        for (int i=1;i<=n;i++){
+            if(i%3==0 && i%5!=0) {res.add("Fizz");continue;}
+            if(i%5==0 && i%3!=0) {res.add("Buzz");continue;}
+            if(i%15==0) {res.add("FizzBuzz");continue;}
+            res.add(String.valueOf(i));
+        }
+        return res;
+    }
+}
