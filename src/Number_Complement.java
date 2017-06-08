54e0be07372227baf4ffc45f18e36c0702e5a19a
lee-siyang <lee_siyang@163.com>
2017-06-09 00:04:01 +0800

    comment2

---
+++ src/Number_Complement.java
@@ -0,0 +1,19 @@
+/**
+ * Created by lisiyang on 17/6/8.
+ */
+public class Number_Complement {
+    public static void main(String[] args) {
+        int n = 3;
+        int res = findComplement(n);
+        System.out.println(res);
+    }
+    public static int findComplement(int num) {
+        if(num==0) return 0;
+        if(num<0) return ~num;
+        int mask=1;
+        while (mask < num){
+            mask<<=1;
+            mask|=1;
+        } return mask^num;
+    }
+}
