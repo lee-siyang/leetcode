e50183a70ec35b0eb115f7e49ddd9389d5c2a42e
lee-siyang <lee_siyang@163.com>
2017-06-09 22:49:48 +0800

    344. Reverse String

---
+++ src/Reverse_String.java
@@ -0,0 +1,18 @@
+/**
+ * Created by lisiyang on 17/6/9.
+ */
+public class Reverse_String {
+    public static void main(String[] args) {
+        String s ="hello";
+        //String res = reverseString(s);
+        System.out.println(reverseString(s));
+    }
+    public static String reverseString(String s) {
+        int len=s.length();
+        char[] res = new char[len];
+        for(int i=0;i<=len-1;i++){
+            res[i]= s.charAt(len-1-i);
+        }
+        return new String(res);
+    }
+}
