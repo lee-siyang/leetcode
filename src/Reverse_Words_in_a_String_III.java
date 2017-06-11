343629e14287df6fa56429b37faeb91d9afc3b01
lee-siyang <lee_siyang@163.com>
2017-06-11 23:14:05 +0800

    557. Reverse Words in a String III https://leetcode.com/problems/reverse-words-in-a-string-iii/#/description

--- src/Reverse_Words_in_a_String_III.java
+++ src/Reverse_Words_in_a_String_III.java
@@ -4,16 +4,36 @@
 public class Reverse_Words_in_a_String_III {
     public static void main(String[] args) {
         String s ="Let's take LeetCode contest";
+//        System.out.println(s.length());
         //String res = reverseString(s);
         System.out.println(reverseWords(s));
     }
     public static String reverseWords(String s) {
-        int len=s.length();
-        char[] res = new char[len];
-        for(int i=0;i<=len-1;i++){
-            res[i]= s.charAt(len-1-i);
+        String[] strs = s.split(" ");
+        String reverse = "";
+        //int len = 0;
+        for (String str:strs){
+//            System.out.println(str);
+            int len = str.length();
+//            System.out.println(len);
+            char[] res = new char[len];
+            for (int i = 0; i <= len - 1; i++) {
+                res[i] = str.charAt(len - 1 - i);
+            }
+//            System.out.println(new String(res));
+            reverse = reverse+ new String(res)+" ";
         }
-        return new String(res);
+//        System.out.println(reverse.substring(0,reverse.length()).length());
+//        for(int j=0;j<strs.length;j++) {
+//            String str = strs[j];
+//            int len = str.length();
+//            char[] res = new char[len];
+//            for (int i = 0; i <= len - 1; i++) {
+//                res[i] = s.charAt(len - 1 - i);
+//            }
+//            reverse = reverse+ new String(res)+" ";
+//        }
+        return reverse.substring(0,reverse.length()-1);
 //        String words=s;
 //        for (int i=0;i<len/2;i++)
 //            words=s.replace(s.charAt(i),s.charAt(len-1-i));
