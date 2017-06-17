f4484668a179d977f2b52bf6510740644f5366cf
lee-siyang <lee_siyang@163.com>
2017-06-17 13:27:19 +0800

    292. Nim Game

--- src/Nim_Game.java
+++ src/Nim_Game.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Created by lisiyang on 17/6/17.
  * https://leetcode.com/problems/nim-game/#/description
  * 292. Nim Game
  */
 public class Nim_Game {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input an Integer,Quit with others");
+        while (in.hasNextInt()){
+            int n = in.nextInt();
+            if(canWinNim(n)) System.out.println("You win!");
+            else System.out.println("Try again?");
+        }
+
+    }
+    public static boolean canWinNim(int n) {
+        boolean res = false;
+        for(int i = 1;!res && i<4;i++){
+            if((n-i)%4==0) res=true;
+        }
+        return res;
+    }
 }
