e610172647e4aaf6254cd04525e2ad7fb89f80b0
lee-siyang <lee_siyang@163.com>
2017-06-29 21:47:32 +0800

    217. Contains Duplicate

--- src/Contains_Duplicate.java
+++ src/Contains_Duplicate.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by lisiyang on 17/6/29.
  * https://leetcode.com/problems/contains-duplicate/#/description
@@ -9,13 +12,20 @@ public class Contains_Duplicate {
         System.out.println(containsDuplicate(nums));
     }
     public static boolean containsDuplicate(int[] nums) {
-        for(int i = 0; i < nums.length-1; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] == nums[j]) {
-                    return true;
-                }
-            }
+        Set<Integer> numSet = new HashSet<>();
+        for (int i:nums){
+            if (!numSet.add(i))
+                return true;
         }
         return false;
+//
+//        for(int i = 0; i < nums.length-1; i++) {
+//            for(int j = i + 1; j < nums.length; j++) {
+//                if(nums[i] == nums[j]) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
     }
 }
