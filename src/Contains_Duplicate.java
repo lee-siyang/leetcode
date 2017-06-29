19a5c7614422bc2622caa1d5cf6d85a93a992707
lee-siyang <lee_siyang@163.com>
2017-06-29 21:40:04 +0800

    217. Contains Duplicate

---
+++ src/Contains_Duplicate.java
@@ -0,0 +1,21 @@
+/**
+ * Created by lisiyang on 17/6/29.
+ * https://leetcode.com/problems/contains-duplicate/#/description
+ * 217. Contains Duplicate
+ */
+public class Contains_Duplicate {
+    public static void main(String[] args){
+        int[] nums = {12,3,45,61,12,4};
+        System.out.println(containsDuplicate(nums));
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        for(int i = 0; i < nums.length-1; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
