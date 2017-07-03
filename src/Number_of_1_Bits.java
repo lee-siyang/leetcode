65384e8d336818bac034c5708357dcbaf53078e8
lee-siyang <lee_siyang@163.com>
2017-07-03 20:44:05 +0800

    191. Number of 1 Bits

--- src/Number_of_1_Bits.java
+++ src/Number_of_1_Bits.java
@@ -7,5 +7,12 @@ public class Number_of_1_Bits {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         return Integer.bitCount(n);
+//        下面这个也行,用不考虑符号的右移比较省时间
+//        int sum=0;
+//        while (n!=0){
+//            sum += (n&1);
+//            n=n>>>1;
+//        }
+//        return sum;
     }
 }
