cf1bfc7a1326d418281af8d70c187d11a2c64522
lee-siyang <lee_siyang@163.com>
2017-07-10 13:21:14 +0800

    367. Valid Perfect Square

---
+++ src/Valid_Perfect_Square.java
@@ -0,0 +1,22 @@
+/**
+ * Created by lisiyang on 17/7/10.
+ * https://leetcode.com/problems/valid-perfect-square/#/description
+ * 367. Valid Perfect Square
+ */
+public class Valid_Perfect_Square {
+    public boolean isPerfectSquare(int num) {
+        int i=1;
+        while (num>0){
+            num-=i;
+            i+=2;
+        }
+        return num==0;
+//        if (num<1) return false;
+//        int i=1;
+//        while (i*i<num) i++;
+//        if (i*i==num){
+//            return true;
+//        }
+//        return false;
+    }
+}
