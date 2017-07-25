23ba810f32e7bbdc68d2e774de897015c8e608ea
lee-siyang <lee_siyang@163.com>
2017-07-25 09:26:33 +0800

    400. Nth Digit

--- src/Nth_Digit.java
+++ src/Nth_Digit.java
@@ -1,8 +1,20 @@
 /**
  * Created by lisiyang on 17/7/23.
+ * https://leetcode.com/problems/nth-digit/#/description
+ * 400. Nth Digit
  */
 public class Nth_Digit {
     public int findNthDigit(int n) {
-        return 0;
+        int digit = 1;  //digit is the length of the number
+        long counts = 9;  //every length of digit has 9*10^n numbers
+        while (n > digit*counts){
+            n -= digit*counts;
+            digit++;
+            counts *= 10;
+        }
+        int baseNumber = (int)Math.pow(10,digit-1);
+        int exactNumber = (n-1)/digit + baseNumber;
+        int mod = (n-1)%digit;
+        return String.valueOf(exactNumber).charAt(mod)-'0';
     }
 }
