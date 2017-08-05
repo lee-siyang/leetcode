1fdb4fa59e8f01cb3c2d41501fa9aa2d4e19b1da
lee-siyang <lee_siyang@163.com>
2017-08-05 15:26:55 +0800

    599. Minimum Index Sum of Two Lists

--- src/Minimum_Index_Sum_of_Two_Lists.java
+++ src/Minimum_Index_Sum_of_Two_Lists.java
@@ -1,8 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by lisiyang on 17/6/21.
+ * https://leetcode.com/problems/minimum-index-sum-of-two-lists/description/
+ * 599. Minimum Index Sum of Two Lists
  */
 public class Minimum_Index_Sum_of_Two_Lists {
     public static void main(String[] args) {
@@ -11,24 +12,58 @@ public class Minimum_Index_Sum_of_Two_Lists {
         System.out.println(findRestaurant(list1, list2)[0]);
     }
     public static String[] findRestaurant(String[] list1, String[] list2) {
-        ArrayList<String> minList = new ArrayList<>();
-//        int findMin = list1.length+list2.length;
-//        String[] res = new String[Math.min(list1.length,list2.length)];
-        int findMin = list1.length+list2.length;
-        String[] res = new String[Math.min(list1.length,list2.length)];
-        int count = 0;
-        for (int i = 0 ;i< list1.length;i++){
-            for (int j = 0;j<list2.length;j++){
-                if (list1[i].equals(list2[j]) && Math.min(findMin,i+j)==i+j) {
-                    if (i+j==findMin) {
-                        count++;
-                    }
-                    findMin = i+j;
-                    res[count] = list1[i];
+        Map<String, Integer> l1 = new HashMap<>();
+        List<String> result = new LinkedList<>();
+//        Map<String, Integer> l2 = new HashMap<>();
+        for (int i = 0;i<list1.length;i++){
+            l1.put(list1[i],i);
+        }
+//        for (int i =0;i<list2.length;i++){
+//            l2.put(list2[i],i);
+//        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0 ; i<list2.length;i++){
+            Integer j = l1.get(list2[i]);
+            if (j != null && i+j<=min){
+                if (i+j<min){
+                    result = new LinkedList<>();
+                    min = i+j;
                 }
-
+                result.add(list2[i]);
             }
         }
-        return res;
+        return result.toArray(new String[result.size()]);
+//        for (Map.Entry<String, Integer> entry1 :l1.entrySet()){
+//            for (Map.Entry<String, Integer> entry2 : l2.entrySet()){
+//                if (entry1.getKey().equals(entry2.getKey())
+//                        && min >= entry1.getValue()+entry2.getValue()){
+//                    if (min > entry1.getValue()+entry2.getValue()){
+//                        result = new LinkedList<>();
+//                        min = entry1.getValue()+entry2.getValue();
+//                    }
+//                    result.add(entry1.getKey());
+//                }
+//            }
+//        }
+//        return result.toArray(new String[result.size()]);
+//        ArrayList<String> minList = new ArrayList<>();
+////        int findMin = list1.length+list2.length;
+////        String[] res = new String[Math.min(list1.length,list2.length)];
+//        int findMin = list1.length+list2.length;
+//        String[] res = new String[Math.min(list1.length,list2.length)];
+//        int count = 0;
+//        for (int i = 0 ;i< list1.length;i++){
+//            for (int j = 0;j<list2.length;j++){
+//                if (list1[i].equals(list2[j]) && Math.min(findMin,i+j)==i+j) {
+//                    if (i+j==findMin) {
+//                        count++;
+//                    }
+//                    findMin = i+j;
+//                    res[count] = list1[i];
+//                }
+//
+//            }
+//        }
+//        return res;
     }
 }
