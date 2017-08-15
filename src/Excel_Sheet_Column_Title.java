ed05300d97853c5738e87652c842d60b18d502b7
lee-siyang <lee_siyang@163.com>
2017-08-15 23:07:54 +0800

    404. Sum of Left Leaves

---
+++ src/Excel_Sheet_Column_Title.java
@@ -0,0 +1,16 @@
+/**
+ * Created by lisiyang on 17/8/15.
+ * https://leetcode.com/problems/excel-sheet-column-title/description/
+ * 168. Excel Sheet Column Title
+ */
+public class Excel_Sheet_Column_Title {
+    public String convertToTitle(int n) {
+        StringBuilder str = new StringBuilder();
+        while (n>0){
+            n--;
+            str.insert(0,(char)(n%26+'A'));
+            n /= 26;
+        }
+        return new String(str);
+    }
+}
