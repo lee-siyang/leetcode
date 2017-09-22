a87252535adfcc3b0694c0c1a3c7c708965f480e
lee-siyang <lee_siyang@163.com>
2017-09-22 13:12:02 +0800

    535. Encode and Decode TinyURL

---
+++ src/Codec.java
@@ -0,0 +1,16 @@
+/**
+ * Created by lisiyang on 17/9/22.
+ * https://leetcode.com/problems/encode-and-decode-tinyurl/description/
+ * 535. Encode and Decode TinyURL
+ */
+public class Codec {
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+
+    }
+}
